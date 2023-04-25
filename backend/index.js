985b561eb8e612fa11dc26e8dc4de939ab65d012
skad12 <dammykunle79@gmail.com>
2023-04-25 17:58:04 +0100

    node-chat-app

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const {default: axios} = require("axios")
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      {
+        username: username,
+        secret: username,
+        first_name: username,
+      },
+      {
+        headers: { "private-key": "aeb6b996-8fc1-4e0f-8de8-133f876a14e8" },
+      }
+    )
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.listen(3000);
